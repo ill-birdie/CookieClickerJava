494ede56ce7b673dcb2521cb9618dbf3a718c849
Ava <avahom16@gmail.com>
2026-01-13 16:44:56 -0500

    Reformatted DialogueHandler/Reader to Dialogue

---
+++ src/Dialogue.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Dialogue {
+
+    public Dialogue() { }
+
+    public static ArrayList<String> getFileText(String path) {
+        ArrayList<String> fileText = new ArrayList<>();
+        try {
+            File file = new File(path);
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                fileText.add(fileReader.nextLine());
+            }
+            fileReader.close();
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found");
+        }
+        return fileText;
+    }
+
+    public static String promptUser(String prompt) {
+        Scanner consoleReader = new Scanner(System.in);
+        System.out.print(prompt);
+        return consoleReader.nextLine().strip().toLowerCase();
+    }
+
+    public static void displayAt(String path) {
+        ArrayList<String> text = getFileText(path);
+        boolean skipped = false;
+        for (String line : text) {
+            System.out.print(line);
+            if (!skipped) {
+                if (promptUser(" ").equals("skip")) {
+                    skipped = true;
+                }
+            }
+            else {
+                System.out.println();
+            }
+        }
+    }
+}
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,15 +1,13 @@
 public class GameLogic {
-    DialogueHandler dialogue;
     Player player;
     Cookie cookie;
 
     public GameLogic() {
-        this.dialogue = new DialogueHandler();
         this.player = new Player();
         this.cookie = new Cookie();
     }
 
     public void play() {
-        dialogue.displayDialogue("src/dialogue/intro.txt");
+        Dialogue.displayAt("src/dialogue/intro.txt");
     }
 }
