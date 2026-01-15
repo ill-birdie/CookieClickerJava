0c6a4bd3e91007bce1b3175edbbd22189cb1bb1d
Ava <avahom16@gmail.com>
2026-01-15 18:17:21 -0500

    Reformatted commands and added buying functionality

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,38 +1,60 @@
 public class GameLogic {
     Player player;
     Cookie cookie;
+    UpgradeList upgrades;
 
     public GameLogic() {
         this.player = new Player();
         this.cookie = new Cookie();
+        this.upgrades = new UpgradeList();
+    }
+
+    public String getCommands() {
+        return """
+                List of commands:\
+                
+                stats : displays user statistics\
+                
+                buy : asks for a name, then buys the upgrade if possible\
+                
+                upgrades : displays unbought upgrades\
+                
+                rebirth : allows the user to rebirth, resetting cookies for a higher base multiplier\
+                
+                quit : quits the simulation\
+                
+                """;
+    }
+
+    public String getStats() {
+        return  "-- USER STATS --" +
+                "\nCookies: " + player.getNumCookies() +
+                "\nHighest cookies: " + player.getMaxCookies() +
+                "\nNumber of cookie clicks: " + cookie.getTimesClicked() +
+                "\nRebirths: " + player.getNumRebirths();
     }
 
     public void executeCommand(String command) {
         System.out.println();
         switch (command.substring(0, 1)) {
-            case "c" -> {
-                System.out.println("""
-                        List of commands:\
-                        
-                        stats : displayers user statistics\
-                        
-                        upgrades : displays the upgrade tree\
-                        
-                        rebirth : allows the user to rebirth, resetting cookies for a higher base multiplier\
-                        
-                        quit : quits the simulation\
-                        
-                        """);
-            }
-            case "s" -> {
-                System.out.println("-- USER STATS --" +
-                        "\nCookies: " + player.getNumCookies() +
-                        "\nHighest cookies: " + player.getMaxCookies() +
-                        "\nNumber of cookie clicks: " + cookie.getTimesClicked() +
-                        "\nRebirths: " + player.getNumRebirths());
+            case "c" -> System.out.println(getCommands());
+
+            case "s" -> System.out.println(getStats());
+
+            case "b" -> {
+                System.out.println(upgrades.getUnbought());
+                String targetUpgrade = Dialogue.promptUser("Which upgrade would you like to buy? (Strictly typed): ");
+                if (upgrades.upgradeUnbought(targetUpgrade)) {
+                    upgrades.buyUpgrade(targetUpgrade);
+                    System.out.println("Bought!");
+                } else {
+                    System.out.println("Upgrade not found in unbought upgrades: \"" + targetUpgrade + "\"");
+                }
+                System.out.println();
             }
             case "u" -> {
-                System.out.println("Viewing upgrade tree");
+                System.out.println("-- Unbought upgrades: --");
+                System.out.println(upgrades.getUnbought());
             }
             case "r" -> {
                 String rebirthOutput;
@@ -45,9 +67,7 @@ public class GameLogic {
                 }
                 System.out.println(rebirthOutput);
             }
-            default -> {
-                System.out.println("Unknown command: \"" + command + "\"");
-            }
+            default -> System.out.println("Unknown command: \"" + command + "\"");
         }
     }
 
@@ -59,7 +79,7 @@ public class GameLogic {
             if (!lastWasCommand) {
                 prompt = "Cookies: " + player.getNumCookies() + " | ";
             } else {
-                prompt = "Press [ENTER] to continue clicking: ";
+                prompt = "Press [ENTER] to continue clicking, or type a different command: ";
             }
             userInput = Dialogue.promptUser(prompt);
             if (userInput.equals("quit")) {
@@ -69,7 +89,10 @@ public class GameLogic {
                 executeCommand(userInput);
                 lastWasCommand = true;
             } else {
-                player.incrementCookies(cookie.click(player));
+                int baseClick = cookie.click();
+                int upgradesMulti = upgrades.getMultiplier();
+                int rebirthMulti = (int) Math.pow(2, player.getNumRebirths());
+                player.incrementCookies(baseClick * upgradesMulti * rebirthMulti);
                 lastWasCommand = false;
             }
         }
