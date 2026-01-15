3c30558d7261c59e7ff7e30c6880b238feb1dc40
Birdie <sample@bths.edu>
2026-01-15 10:44:35 -0500

    Continued editing

--- src/UpgradeTree.java
+++ src/UpgradeTree.java
@@ -1,26 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-record Attributes(String type, double multiplier, int cost) { }
+record Attributes(boolean isOwned, String type, double multiplier, int cost) { }
 
 public class UpgradeTree {
     private HashMap<String, Attributes> upgrades;
-    private ArrayList<String> owned;
     private double totalMultiplier;
 
     public UpgradeTree() {
         this.upgrades = new HashMap<>();
-        this.upgrades.put("First Steps", new Attributes("Base", 2.0, 0));
-        this.upgrades.put("Triple digits!", new Attributes("Base", 1.5, 100));
-        this.owned = new ArrayList<>();
+        this.upgrades.put("First Steps", new Attributes(false, "Base", 2.0, 0));
+        this.upgrades.put("Triple digits!", new Attributes(false, "Base", 1.5, 100));
         this.totalMultiplier = 1;
+    }
+
+    public void buyUpgrade(String name) {
+        if (this.upgrades.containsKey(name)) {
+            this.upgrades.put("First Steps", new Attributes(true, "Base", 2.0, 0));
+        }
     }
 
     public void updateMultiplier() {
         double newMultiplier = 1;
-        for (String upgrade : owned) {
-            Attributes attributes = upgrades.get(upgrade);
-             if (attributes.type().equals("Base")) {
+        for (String upgrade : this.upgrades.keySet()) {
+            Attributes attributes = this.upgrades.get(upgrade);
+             if (attributes.isOwned() && attributes.type().equals("Base")) {
                  newMultiplier *= attributes.multiplier();
              }
         }
