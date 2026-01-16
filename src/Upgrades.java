57dd217f5d4d0a5222fec54881397bb5790d8396
Ava <avahom16@gmail.com>
2026-01-16 16:49:15 -0500

    Upgrades are initialized more cleanly; some modified for game balancing

--- src/Upgrades.java
+++ src/Upgrades.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class UpgradeList {
+public class Upgrades {
     private final ArrayList<Upgrade> UPGRADES;
     private int multiplier;
 
-    public UpgradeList() {
-        this.UPGRADES = new ArrayList<>();
-        this.UPGRADES.add(new Upgrade("First Steps", "Base", 2, 0));
-        this.UPGRADES.add(new Upgrade("Quadruple Digits!", "Base", 4, (int) Math.pow(10, 3)));
-        this.UPGRADES.add(new Upgrade("Baby steps", "Base", 2, (int) Math.pow(10, 4)));
-        this.UPGRADES.add(new Upgrade("It just keeps growing", "Base", 2, (int) Math.pow(10, 5)));
-        this.UPGRADES.add(new Upgrade("Basically a million", "Base", 2, 999999));
-        this.UPGRADES.add(new Upgrade("Second Life", "Base", 2, (int) Math.pow(10, 6)));
-        this.UPGRADES.add(new Upgrade("Now do it again", "Base", 2, (int) Math.pow(10, 7)));
+    public Upgrades() {
+        this.UPGRADES = new ArrayList<>(Arrays.asList(
+            new Upgrade("First Steps", "Base", 2, 0),
+            new Upgrade("Triple Digits", "Base", 3, 100),
+            new Upgrade("Quadruple Digits!", "Base", 4, 1000),
+            new Upgrade("Greed Sets In", "Base", 2, 10000),
+            new Upgrade("Your First Drought", "Base", 5, (int) Math.pow(10, 5)),
+            new Upgrade("The Real Long Haul", "Base", 2, (int) (2.5 * Math.pow(10, 5))),
+            new Upgrade("Second Life", "Base", 2, (int) Math.pow(10, 6)),
+            new Upgrade("Death Can't Stop Me From Doing It Again", "Base", 2, (int) Math.pow(10, 7))
+        ));
         this.multiplier = 1;
     }
 
@@ -25,6 +28,13 @@ public class UpgradeList {
         updateMultiplier();
     }
 
+    /**
+     * Finds the index of an Upgrade by name.
+     * Upgrades can only be identified through their attributes.
+     * Therefore, this method searches each Upgrade and returns the Upgrade with a name that matches the argument name.
+     * @param name The name of the Upgrade.
+     * @return The index of the Upgrade.
+     */
     public int getUpgradeIdx(String name) {
         int idx = -1;
         if (name == null || name.isEmpty()) {
@@ -32,7 +42,7 @@ public class UpgradeList {
         }
         for (int i = 0; i < this.UPGRADES.size(); i++) {
             Upgrade u = this.UPGRADES.get(i);
-            if (name.equals(u.getName().toLowerCase())) {
+            if (name.equalsIgnoreCase(u.getName())) {
                 return i;
             }
         }
@@ -50,7 +60,7 @@ public class UpgradeList {
     public void updateMultiplier() {
         int newMulti = 1;
         for (Upgrade u : this.UPGRADES) {
-            if (u.isOwned() && u.getType().equals("Base")) {
+            if (u.isOwned() && u.getType().equalsIgnoreCase("Base")) {
                 newMulti *= u.getMultiplier();
             }
         }
